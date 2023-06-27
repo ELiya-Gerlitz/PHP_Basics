d216c5b3c134adca846f275d1d74713d11c4dbf8
Eliya Gerlitz <tent84e@gmail.com>
2023-06-27 15:36:11 +0300

    timezone

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
 <?php
 
 require_once "./calc.php";
@@ -75,3 +84,19 @@ $employee->name = "Moshe"; //object (Literal Object) field-name WITHOUT $!!!
 $employee->rank = "senior";
 
 echo "Employee Name: $employee->name, Salary: $employee->salary, rank: $employee->rank ";
+
+echo "<br>";
+
+function showTime(){
+    $time = new DateTimeZone("Asia/Jerusalem");
+    $currentTime = new DateTime("now", $time);
+    return $currentTime->format("d/m/Y H:i:s") ;
+}
+echo showTime();
+echo "<br>";
+
+
+?>
+    
+</body>
+</html>
